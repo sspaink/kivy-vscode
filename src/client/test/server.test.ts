338a3ffab1729ad5ec66594236111011df3db435
BattleBas <battlebas@users.noreply.github.com>
2017-10-27 01:19:26 -0500

    Introducing unit tests with Mocha and Chai
    * Created unit tests for the diagnostic functions in server.ts
    * Converted spaces to tabs (to align with vscode coding standard)

---
+++ src/client/test/server.test.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+import { checkForKivyHeader } from '../../server/src/server';
+import { expect } from 'chai';
+import { Diagnostic } from '../../server/node_modules/vscode-languageserver';
+import 'mocha';
+
+describe('Check for Kivy header', () => {
+
+	it('diagnostic should contain a message', () => {
+		let diagnostics: Diagnostic[] = [];
+		let firstLine: string = "";
+
+		checkForKivyHeader(firstLine, diagnostics);
+		expect(diagnostics[0].message).to.equal('Kivy files require #:kivy !');
+	});
+
+});
